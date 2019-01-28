1c80f415c142297bed32b16b44c480d3cbbc4546
Jair Pereira Leonardo Júnior <jairnetjunior@hotmail.com>
2019-01-28 16:40:47 -0300

    Atualização: utilização de somente @JsonIgnore

--- src/main/java/com/jair/cursomc/domain/PagamentoComBoleto.java
+++ src/main/java/com/jair/cursomc/domain/PagamentoComBoleto.java
@@ -14,7 +14,7 @@ public class PagamentoComBoleto extends Pagamento{
 	@JsonFormat(pattern="dd/MM/yyyy")
 	private Date dataVencimento;
 	
-	@JsonFormat(pattern="dd/MM/yyyy HH:mm")
+	@JsonFormat(pattern="dd/MM/yyyy")
 	private Date dataPagamento;
 	
 	
