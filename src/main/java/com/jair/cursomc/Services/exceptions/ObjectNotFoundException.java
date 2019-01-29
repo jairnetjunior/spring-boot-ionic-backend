7ce977fcda181860c3ac7bb6b04220536722c3e1
Jair Pereira Leonardo Júnior <jairnetjunior@hotmail.com>
2019-01-29 15:45:52 -0300

    Deletando uma Categoria

--- src/main/java/com/jair/cursomc/Services/exceptions/ObjectNotFoundException.java
+++ src/main/java/com/jair/cursomc/Services/exceptions/ObjectNotFoundException.java
@@ -2,9 +2,6 @@ package com.jair.cursomc.services.exceptions;
 
 public class ObjectNotFoundException extends RuntimeException{
 
-	/**
-	 * 
-	 */
 	private static final long serialVersionUID = 1L;
 	
 	public ObjectNotFoundException(String msg) {
